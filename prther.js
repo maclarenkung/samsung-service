c0550a648c1d19a5a42711f98c83029aef0b98b1
KG <maclarenkung@gmail.com>
2020-08-27 11:17:13 +0700

    closer

--- prther.js
+++ prther.js
@@ -120,7 +120,7 @@ const callSlider = () => {
     var swiper2 = new Swiper(".s2", {
       slidesPerView: 1.3,
       spaceBetween: 100,
-      loop: false,
+      loop: true,
       pagination: {
         el: ".swiper-pagination,.swiper-paginations",
         clickable: true,
@@ -134,23 +134,61 @@ const callSlider = () => {
    
 }
 
+const callSlider2 = () => {
+  var swiper2 = new Swiper(".s2", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination,.swiper-paginations",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+}
+// if (matchMedia) {
+//   const mq = window.matchMedia("(min-width: 768px)");
+//   mq.addListener(WidthChange);
+//   WidthChange(mq);
+// }
+// function WidthChange(mq) {
+//   if (mq.matches) {
+//     console.log('yeahhhssssshhh');
+//   }
+//   else {
+//     console.log('yeahhhhhh');
+//   }
+// }
+
+// if (mq.matches) {
+//   renderSlider()
+// }
+// else {
+//   renderSlider2()
+// }
 const renderSlider = (name) =>{
 
+  
     let templateHtml = (data) => {
-        if (subActive == 'Unbox' || subActive == 'Inquiry' ){
-            return ` 
-        <div class="swiper-slide dt-show">
+
+        if (subActive == 'Unbox' || subActive == 'Inquiry') {
+          return ` 
+        <div class="swiper-slide slide--01">
            <div class="slide--item">
-               <img src="images/${data.image}.png" alt=""  width="100%"/>
+               <img src="images/${data.image}.png" alt=""  width="100%" class="dt-show"/>
+               <img src="images/${data.image2}.png" alt=""  width="100%" class="mb-show"/>
             </div>
         </div>
-        <div class="swiper-slide mb-show">
-           <div class="slide--item">
-               <img src="images/${data.image2}.png" alt=""  width="100%"/>
-            </div>
-        </div>
+         
+      
         `;
         }
+      
     } 
     let render = ''
     items[name][subActive].forEach(el=>{
@@ -158,6 +196,7 @@ const renderSlider = (name) =>{
     })
     return render
 }
+
 
 
 $(window).ready(()=>{
@@ -168,7 +207,20 @@ $(window).ready(()=>{
         content.html(renderSlider(mainActive))
         console.log(renderSlider(mainActive));
         console.log(dataSlider);
-        callSlider()
+
+          if (matchMedia) {
+            const mq = window.matchMedia("(min-width: 768px)");
+            mq.addListener(WidthChange);
+            WidthChange(mq);
+          }
+          function WidthChange(mq) {
+            if (mq.matches) {
+              callSlider()
+            }
+            else {
+              callSlider2()
+            }
+          }
 
     })
 
@@ -177,7 +229,19 @@ $(window).ready(()=>{
         subActive = dataTabname
         let content = $(`#${subActive} .swiper-wrapper`)
         content.html(renderSlider(mainActive))
-        callSlider()
+      if (matchMedia) {
+        const mq = window.matchMedia("(min-width: 768px)");
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+      }
+      function WidthChange(mq) {
+        if (mq.matches) {
+          callSlider()
+        }
+        else {
+          callSlider2()
+        }
+      }
 
     })
 
